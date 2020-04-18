95522c0dee48f434bf502b6334a68a80a7c2d37b
Marko Laharnar <marko.laharnar@gmail.com>
2020-04-18 18:11:53 +0200

    Meteors die after a while

--- SeletonSurvior/Assets/Common/Camera/AimAtTransform.cs
+++ SeletonSurvior/Assets/Common/Camera/AimAtTransform.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.WSA.Input;
 
 public class AimAtTransform : MonoBehaviour
 {
@@ -8,6 +10,25 @@ public class AimAtTransform : MonoBehaviour
 
     [Range(0.0f, 1.0f)]
     public float strength;
+
+    public bool fullAimOnStart = false;
+
+    public Vec3VarRef targetPositionOnStart;
+    public FloatVarRef distanceToTargetAtStart;
+
+    public UnityEvent onStartEnd;
+
+    void Start()
+    {
+        if (fullAimOnStart)
+        {
+            transform.up = target.position - transform.position;
+        }
+
+        targetPositionOnStart.Value = target.position;
+        distanceToTargetAtStart.Value = Vector2.Distance(target.position, transform.position);
+        onStartEnd?.Invoke();
+    }
 
     // Update is called once per frame
     void Update()
---
+++ SeletonSurvior/Assets/Common/Camera/DieAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DieAfterTime:MonoBehaviour
+{
+    public bool beginOnStart = true;
+    public FloatVarRef dieAfterTime;
+
+    void Start()
+    {
+        if (beginOnStart)
+        {
+            Begin();
+        }
+    }
+    public void Begin()
+    {
+        Destroy(gameObject, dieAfterTime.Value);
+    }
+}
