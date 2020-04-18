a65315577e07f64ad4180c6f377e1ce421f88f2e
Marko Laharnar <marko.laharnar@gmail.com>
2020-04-18 17:11:37 +0200

    Simple Follow camera

---
+++ SeletonSurvior/Assets/Common/Camera/FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    [Range(0.0f, 1.0f)]
+    public float followStrength = 0.8f;
+
+    public bool keepX, keepY, keepZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = Vector3.Lerp(transform.position, target.position, followStrength);
+        Vector3 dir = v-transform.position;
+        if (keepX)
+        {
+            dir.x = 0;
+        }
+        if (keepY)
+        {
+            dir.y = 0;
+        }
+        if (keepZ)
+        {
+            dir.z = 0;
+        }
+        transform.Translate(dir*Time.deltaTime*30);
+    }
+}
